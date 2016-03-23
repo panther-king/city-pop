9dc8300ef1a2f33a31fde868ef58ab44574d04e5
Taro Yamashita <taro.ymst@gmail.com>
2016-03-24 07:37:11 +0900

    CLI引数のパース処理を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate getopts;
+extern crate rustc_serialize;
+
+use getopts::Options;
+use std::env;
+
+fn print_usage(program: &str, opts: Options) {
+    println!("{}", opts.usage(&format!("Usage: {} [options] <data-path> <city>", program)));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Show this usage message.");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => { panic!(e.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let data_path = args[1].clone();
+    let city = args[2].clone();
+
 }
